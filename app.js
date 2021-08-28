9af9fb2f3dfb2a110cbf51941bf097820be605bd
harsh-dev14 <devharsh1403@gmail.com>
2021-08-28 16:52:33 +0530

    updated
    Signed-off-by: harsh-dev14 <devharsh1403@gmail.com>

--- app.js
+++ app.js
@@ -1,12 +1,24 @@
 const express = require('express')
 const dotenv = require('dotenv')
 const connectDB = require('./config/db')
+const morgan= require('morgan')
+const exphps = require('express-handlebars')
 //load config
 dotenv.config({path :'./config/config.env'})
 
 connectDB()
 
 const app = express()
+
+//Logging
+
+if(process.env.NODE_ENV==='development'){
+    app.use(morgan('dev'))
+}
+
+//HANdleBars
+app.engine('hbs',exphps({defaultLayout:'main' ,extname:'.hbs'}));
+app.set('view engine', '.hbs');
 
 const PORT = process.env.PORT || 5000
 
